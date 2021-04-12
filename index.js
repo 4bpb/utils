cbd946e86f50c5c926a6e28e347e9f07582e05ea
4bpb <blakebein@gmail.com>
2021-04-12 13:21:01 -0700

    Updated Usage

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ var log = require('./logger.js')
 var toHTML = require('./toHTML.js')
 var toLog = require('./toLog.js')
 var accurateTime = require('./accurateTime.js')
+var toDiscord = require('./toDiscord.js')
 
 
 
+
+//LOG = log('Message Contents','Type of log')
+log('This is a Success log','ok')
+log('This is a Error log','err')
+
+//toHtml = toHTML(fileName, Rawhtml)
+toHTML('website.html','<p>Sample Paragraph</p>')
+
+//ToLog = toLog(data)
+toLog('User requested x data')
+
+//AccurateTime = Time from time.is w/ delay
 accurateTime()
 
+//toDiscord = toDiscord('Message Contents','Type of Message')
+// Will error if you dont have config.json filled out
+toDiscord('The Test worked','ok')
 
